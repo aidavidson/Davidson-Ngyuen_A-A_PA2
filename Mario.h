6e7463b649a38674378b7978ed195724f1d5ed1c
aidavidson <aidavidson@chapman.edu>
2026-09-09 16:07:47 -0700

    updated the mario.h and made sure that the methods were implemented properly

--- Mario.h
+++ Mario.h
@@ -8,9 +8,13 @@ class Mario{
         int V;
         int coins;
         std::string powerLevel;
+        std::string* powerLevels;
     public:
+        Mario(int lives);
+        ~Mario();
         std::string increasePower(std::string currentPower);
-        
+        std::string decreasePower(std::string currentPower);
+        std::string modPower(std::string object);
 };
 
 #endif
