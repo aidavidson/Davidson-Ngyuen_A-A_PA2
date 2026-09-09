fd5f88ec3d1af04a54199d81a9770af14a49179e
AnadyNguyen <annguyen@chapman.edu>
2026-09-09 16:21:16 -0700

    trying to push:

--- Level.h
+++ Level.h
@@ -5,15 +5,8 @@ class Level {
     private:
         char** grid;
     public:
-<<<<<<< HEAD
         Level(char coin, char mushroom, char goomba, char koopaTroopa, char boss, char nothin, bool yesBoss);
         ~Level();
-=======
-        Level(char coin, char mushroom, char goomba, char koopaTroopa, char boss, char nothin, bool yesBoss) {
-
-        };
-        ~Level() {};
->>>>>>> 163be544d45ea69fde1fe6a25d56cc80aaef8c0c
 };
 
 
