4d27a2e389ef5142d5e9671385f04123fd798db8
AnadyNguyen <annguyen@chapman.edu>
2026-09-09 15:44:39 -0700

    edited level

--- Level.h
+++ Level.h
@@ -3,13 +3,17 @@
 
 class Level {
     private:
-
+        int coin;
+        int mushroom;
+        // add enemies for later when we have enemy class
+        // Enemy goomba;
+        // Enemy koopaTroopa;
     public:
         Level() {
-            int **arr = (int **)malloc(rows * sizeof(int *));
+
         };
         ~Level() {
-            
+
         };
 };
 
