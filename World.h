308b15e4500fdcb895756b2f4edb30d61d59293e
AnadyNguyen <annguyen@chapman.edu>
2026-09-09 17:43:57 -0700

    bare world bare level

---
+++ World.h
@@ -0,0 +1,14 @@
+#ifndef WORLD_H
+#define WORLD_H
+#include "Level.h" //no s since Level only one object
+
+class World {
+    private:
+    public:
+        World();
+        ~World();
+
+}
+
+
+#endif
