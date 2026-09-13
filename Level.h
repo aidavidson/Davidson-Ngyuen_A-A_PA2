17afb56999859c9cd8e07c96de43dad7c58e4046
AnadyNguyen <annguyen@chapman.edu>
2026-09-12 19:10:12 -0700

    fixed merge conflicts

--- Level.h
+++ Level.h
@@ -13,6 +13,7 @@ class Level {
         char** grid;
         bool warpPipe = false;
         bool complete = false;
+        bool lastLevel = false;
         char c, m, g, k ,b ,x ,w ,n; //c: coin, m: mushroom, g: goomba, k: koopaTroopa, b: level boss, x: nothing, w: warp pipe, H: mario
     public:
         Level();
